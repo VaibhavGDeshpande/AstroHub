de4b3884ae1099dc5daf073e1142e66f63114b22
Vaibhav Ganesh Deshpande <135240501+VaibhavGDeshpande@users.noreply.github.com>
2025-09-28 19:33:33 +0530

    Update route.ts

--- src/app/api/nasa/apod/route.ts
+++ src/app/api/nasa/apod/route.ts
@@ -1,7 +1,7 @@
 // app/api/nasa/apod/route.ts
 import { NextResponse } from 'next/server';
 
-export const dynamic = 'force-dynamic';
+export const dynamic = 'force-static';
 export const runtime = 'nodejs';
 
 export async function GET(request: Request) {
